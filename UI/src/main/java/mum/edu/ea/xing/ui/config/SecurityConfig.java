baa64c127611b9d7468772515bea6800ff5ab10e
jiangyj <jij000@gmail.com>
2019-07-16 21:43:38 -0500

    change

--- UI/src/main/java/mum/edu/ea/xing/ui/config/SecurityConfig.java
+++ UI/src/main/java/mum/edu/ea/xing/ui/config/SecurityConfig.java
@@ -26,7 +26,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests().antMatchers("/*").hasAnyRole("ADMIN","USER").and()
+                .authorizeRequests().antMatchers("/register").permitAll()
+                .antMatchers("/*").hasAnyRole("ADMIN","USER").and()
                 .formLogin().loginPage("/login").permitAll().and()
                 .logout();
     }
